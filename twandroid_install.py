a42bb47262fa7bfd100297ff0db81d83ebeb1bc4
Oveek <moveek@gmail.com>
2010-11-16 02:51:04 -0800

    Added modified copytree that doesn't copy file permission bits.
    shutil.copytree() uses copy2() and copystat() functions which
    copy file permission bits and metadata. Regular Android users
    don't have permission to chmod files.

--- twandroid_install.py
+++ twandroid_install.py
@@ -10,22 +10,79 @@
     distutils.util respectively.
 
 """
+import android
 import sys
 import os
 import re
 import shutil
 
+droid = android.Android()
+
 # Location of extra python libraries. Additional
 # packages should be installed here.
 PYTHON_FOR_ANDROID='com.googlecode.pythonforandroid'
-TARGET_DIR = find_install_target()
+#TARGET_DIR = find_install_target()
 #TARGET_DIR = os.path.join(os.getcwd(), 'target')
+TARGET_DIR = [ path for path in sys.path if path.find(PYTHON_FOR_ANDROID) != -1 ][0]
+TARGET_DIR = os.path.join(TARGET_DIR, 'apple')
+
+def alert(msg):
+  title = 'twandroid install'
+  message = msg
+  droid.dialogCreateAlert(title, message)
+  droid.dialogSetPositiveButtonText('Continue')
+  droid.dialogShow()
+  response = droid.dialogGetResponse().result
+  return response['which'] == 'positive'
+
 
 print "target: %s" % TARGET_DIR
+alert("located target")
 
 def find_install_target():
     """Check sys.path to find out where python_extras are installed"""
     return [ path for path in sys.path if path.find(PYTHON_FOR_ANDROID) != -1 ][0]
+
+def copytree(src, dst, symlinks=False, ignore=None):
+    """
+    Modified version of copytree() from shutil that
+    that doesn't copy permission bits or file metadata.
+    
+    Regular user under Android doesn't have permission to 
+    chmod, so this version of copytree() uses copyfile() 
+    instead of copy2() and doesn't call copystat().
+    """
+    names = os.listdir(src)
+    if ignore is not None:
+        ignored_names = ignore(src, names)
+    else:
+        ignored_names = set()
+
+    os.makedirs(dst)
+    errors = []
+    for name in names:
+        if name in ignored_names:
+            continue
+        srcname = os.path.join(src, name)
+        dstname = os.path.join(dst, name)
+        try:
+            if symlinks and os.path.islink(srcname):
+                linkto = os.readlink(srcname)
+                os.symlink(linkto, dstname)
+            elif os.path.isdir(srcname):
+                copytree(srcname, dstname, symlinks, ignore)
+            else:
+                shutil.copyfile(srcname, dstname)
+            # XXX What about devices, sockets etc.?
+        except (IOError, os.error), why:
+            errors.append((srcname, dstname, str(why)))
+        # catch the Error from the recursive copytree so that we can
+        # continue with other files
+        except shutil.Error, err:
+            errors.extend(err.args[0])
+
+    if errors:
+        raise Error(errors)
 
 # Lifted from setuptools
 def find_packages(where='.', exclude=()):
@@ -103,9 +160,9 @@ def copy_into(src, dst):
         copy_from = os.path.join(src,f)
         copy_to = os.path.join(dst, f)
         if os.path.isdir(copy_from):
-            shutil.copytree(copy_from, copy_to)
+            copytree(copy_from, copy_to)
         else:
-            shutil.copy(copy_from, copy_to)
+            shutil.copyfile(copy_from, copy_to)
 
 def find_package(setup, found_package):
     """
@@ -115,6 +172,7 @@ def find_package(setup, found_package):
     """
     if found_package:
         print "found_package: %s" % found_package 
+        #alert("found package")
         return get_absolute_path(found_package[0])
     else:
         package = []
@@ -151,9 +209,11 @@ def copy_package(setup):
     """
     top_level_package = find_package(setup, find_packages(exclude=get_exclude_dirs(setup)))
     print "package is: %s" % top_level_package
+    #alert("package is")
     try:
         install_target = get_install_target(os.path.basename(top_level_package))
         print "install_target: %s" % install_target
+        #alert("install is")
         os.stat(install_target)
 
         # If we get here, the install_target already exists. This means
@@ -163,7 +223,16 @@ def copy_package(setup):
 
     except OSError:
         # Target doesn't exist yet, so install the package.
-        shutil.copytree(top_level_package, install_target)
+        print "install_target: %s" % install_target
+        #alert("About to copytree")
+
+        #try: 
+        copytree(top_level_package, install_target)
+        #except OSError:
+        #    print OSError
+        #    pass
+
+        #alert("completed copytree")
         
 def copy_single_module(module_name):
     """
@@ -171,8 +240,9 @@ def copy_single_module(module_name):
     """
     py_module = module_name.group(1) + ".py" 
     absolute_module_path = get_absolute_path(py_module)
-    shutil.copy(absolute_module_path, TARGET_DIR)
+    shutil.copyfile(absolute_module_path, os.path.join(TARGET_DIR, py_module))
     print "copied: %s" % py_module
+    #alert("copied module")
 
 def copy_modules_and_packages(setup):
     """
@@ -207,17 +277,19 @@ def install():
         os.chdir(d)
         print "dir: %s" % d
         print "contents: %s" % os.listdir('.')
+        #alert("listed dir contents")
 
         setup = read_setup()
         if setup:
             copy_modules_and_packages(setup)
             print "package installed"
+            #alert("package installed")
         else:
             print "not a python package: skipping..."
             
         
         os.chdir("../")
 
-        raw_input()    
+        #raw_input()    
     
 install()
